95d9be2f7f6993244f48db0656d1efddb8d90704
mystique09 <knightkrypto7@gmail.com>
2022-09-06 22:22:37 +0800

    feat: migrate latest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 
-const MONGO_URI_PROD = "mongodb+srv://mystique09:9nGhsRv1HIr0yiuz@cluster0.tr9fs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const MONGO_URI_DEV = "mongodb://localhost:27017/crud-backend";
+const _MONGO_URI_PROD = "mongodb+srv://mystique09:9nGhsRv1HIr0yiuz@cluster0.tr9fs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGO_URI_DEV = "mongodb://localhost:27017/crud?directConnection=true";
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     debug: true,
     playground: true,
-    autoSchemaFile: 'schema.gql'
+    driver: ApolloDriver,
+    autoSchemaFile: 'schema.gql',
   }),
-  MongooseModule.forRoot(MONGO_URI_PROD),
-  UsersModule, PostsModule],
+  MongooseModule.forRoot(MONGO_URI_DEV, {
+    useUnifiedTopology: true,
+  }),
+    UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
-  
+
 })
-export class AppModule {}
+export class AppModule { }
